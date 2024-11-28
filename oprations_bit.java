bbe302bb574baa7b5b5ed9da75a404022fb81f43
Shubhi89 <shubhidwivedi1415@gmail.com>
2024-11-28 12:06:29 +0530

    adds function

--- oprations_bit.java
+++ oprations_bit.java
@@ -16,7 +16,11 @@ public class oprations_bit {
         int bitMask = ~(1<<i);
         return n & bitMask;
     }
+    public static int clearIBits(int n , int i) {
+        int bitMask = (~0)<<i;
+        return n & bitMask;
+    }
     public static void main(String[] args) {
-        System.out.println(clearIthBit(10, 1));
+        System.out.println(clearIBits(15, 2));
     }
 }
