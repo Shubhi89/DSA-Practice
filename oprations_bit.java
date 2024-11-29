c4036cd2af4498bb37cbbf2650751349cd246631
Shubhi89 <shubhidwivedi1415@gmail.com>
2024-11-29 11:02:15 +0530

    function

--- oprations_bit.java
+++ oprations_bit.java
@@ -20,7 +20,13 @@ public class oprations_bit {
         int bitMask = (~0)<<i;
         return n & bitMask;
     }
+    public static int clearBitsinRange(int n , int i , int j) {
+        int a = ((~0)<<(j+1));
+        int b = (1<<i)-1;
+        int bitMask = a | b;
+        return n & bitMask;
+    }
     public static void main(String[] args) {
-        System.out.println(clearIBits(15, 2));
+        System.out.println(clearBitsinRange(10,2,4));
     }
 }
