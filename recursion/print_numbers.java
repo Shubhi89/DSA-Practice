c9fdc8db793e79789b09ce162641ba6b035fa694
Shubhi89 <shubhidwivedi1415@gmail.com>
2024-12-03 10:58:01 +0530

    practice 2

--- recursion/print_numbers.java
+++ recursion/print_numbers.java
@@ -1,6 +1,6 @@
 package recursion;
 
-public class print_decreasing_numbers {
+public class print_numbers {
     public static void print_dec(int n) {
         if(n == 1) {
             System.out.print(n);
@@ -9,7 +9,15 @@ public class print_decreasing_numbers {
         System.out.print(n +" ");
         print_dec(n-1);
     }
+    public static void print_inc(int n) {
+        if(n == 1) {
+            System.out.print(n + " ");
+            return;
+        }
+        print_inc(n-1);
+        System.out.print(n+" ");
+    }
     public static void main(String[] args) {
-        print_dec(10);
+        print_inc(10);
     }
 }
