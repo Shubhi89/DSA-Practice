da9122150f61929797e4a719f87ebbd24b6d5e34
Shubhi89 <shubhidwivedi1415@gmail.com>
2024-12-07 11:47:13 +0530

    adding comments

--- divide_and_conquer/quick_sort.java
+++ divide_and_conquer/quick_sort.java
@@ -1,21 +1,21 @@
 package divide_and_conquer;
 
 public class quick_sort { // not takes extra space , average=O(nlogn) , worst=O(n^2)
-    public static void quickSort(int arr[] , int si , int ei) {
-        if(si >= ei) {
+    public static void quickSort(int arr[], int si, int ei) {
+        if (si >= ei) {
             return;
         }
         // pivot -> last element
-        int pivotIdx = partition(arr,si,ei);
-        quickSort(arr, si, pivotIdx-1); // left part
-        quickSort(arr, pivotIdx+1, ei); // right part
+        int pivotIdx = partition(arr, si, ei);
+        quickSort(arr, si, pivotIdx - 1); // left part
+        quickSort(arr, pivotIdx + 1, ei); // right part
     }
 
-    public static int partition(int arr[] , int si , int ei) {
+    public static int partition(int arr[], int si, int ei) {
         int pivot = arr[ei];
-        int i = si-1; // to make place for elements smaller than pivot
-        for(int j=si;j<ei;j++) {
-            if(arr[j] <= pivot) {
+        int i = si - 1; // to make place for elements smaller than pivot
+        for (int j = si; j < ei; j++) {
+            if (arr[j] <= pivot) {
                 i++;
                 // swap
                 int temp = arr[j];
@@ -31,14 +31,15 @@ public class quick_sort { // not takes extra space , average=O(nlogn) , worst=O(
     }
 
     public static void printArray(int arr[]) {
-        for(int i=0;i<arr.length;i++) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
-  public static void main(String[] args) {
-    int arr[] = {6,3,9,6,3,4,0,2,1};
-    quickSort(arr, 0, arr.length-1);
-    printArray(arr);
-  }  
+
+    public static void main(String[] args) {
+        int arr[] = { 6, 3, 9, 6, 3, 4, 0, 2, 1 };
+        quickSort(arr, 0, arr.length - 1);
+        printArray(arr);
+    }
 }
