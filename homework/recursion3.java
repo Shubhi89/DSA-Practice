3e246e4acd498ca9f2bd1a9b6bd9d303d4798238
Shubhi89 <shubhidwivedi1415@gmail.com>
2024-12-08 11:20:29 +0530

    homework 3

---
+++ homework/recursion3.java
@@ -0,0 +1,15 @@
+package homework;
+
+public class recursion3 {
+    public static int stringLength(String s) {
+       if(s.length() == 0) {
+        return 0;
+       }
+       return stringLength(s.substring(1)) + 1;
+    }
+    public static void main(String[] args) {
+        String name = "shubhi";
+        System.out.println(stringLength(name));
+
+    }
+}
