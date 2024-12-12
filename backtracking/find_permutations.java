9b730a35c1cdf6fe6d2d058d53a3e9fcdf4bcfef
Shubhi89 <shubhidwivedi1415@gmail.com>
2024-12-12 19:11:47 +0530

    optimize ques code

--- backtracking/find_permutations.java
+++ backtracking/find_permutations.java
@@ -1,7 +1,20 @@
 package backtracking;
 // find and print all permutations of a string 
 public class find_permutations {
+    public static void findPermutation(String str, String ans) { // time = O(n*n!)
+        // base case
+        if(str.length() == 0) {
+            System.out.println(ans);
+        }
+        // recursion
+        for(int i=0;i<str.length();i++) {
+            char curr = str.charAt(i);
+            String newStr = str.substring(0, i) + str.substring(i+1);
+            findPermutation(newStr, ans+curr);
+        }
+    }
     public static void main(String[] args) {
-        
+        String str = "abcd";
+        findPermutation(str,"");
     }
 }
