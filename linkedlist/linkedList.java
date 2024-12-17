5ca037058fe505d69bc2a840a3dd1819ce0bc041
Shubhi89 <shubhidwivedi1415@gmail.com>
2024-12-17 11:24:27 +0530

    added print method

--- linkedlist/linkedList.java
+++ linkedlist/linkedList.java
@@ -10,6 +10,7 @@ public class linkedList {
             this.next = null;
         }
     }
+
     public static Node head;
     public static Node tail;
 
@@ -26,6 +27,7 @@ public class linkedList {
         // step 3
         head = newNode;
     }
+
     public void addLast(int data) { // O(1)
         Node newNode = new Node(data);
         if(head == null) {
@@ -35,12 +37,29 @@ public class linkedList {
         tail.next = newNode;
         tail = newNode;
     }
+
+    public void print() { // O(n)
+        if(head == null) {
+            System.out.println("linked list is empty");
+            return;
+        }
+        Node temp = head;
+        while(temp != null) {
+            System.out.print(temp.data+"-> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
     public static void main(String[] args) {
         linkedList ll = new linkedList();
+        ll.print();
         ll.addFirst(1);
+        ll.print();
         ll.addFirst(2);
+        ll.print();
         ll.addLast(4);
+        ll.print();
         ll.addLast(5);
+        ll.print();
     }
-    
-}
+}    
