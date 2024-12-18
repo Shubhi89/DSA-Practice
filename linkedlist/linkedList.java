6076dc020c6ff08eafd2e2afb50ef9b001ed7f2c
Shubhi89 <shubhidwivedi1415@gmail.com>
2024-12-18 11:37:57 +0530

    added method for recursive search

--- linkedlist/linkedList.java
+++ linkedlist/linkedList.java
@@ -123,6 +123,26 @@ public class linkedList {
         }
         return -1;
     }
+
+    public int helper(Node head , int key) { // both time & space = O(n)
+        if(head == null) {
+            return -1;
+        }
+
+        if(head.data == key){
+            return 0;
+        }
+        int idx = helper(head.next , key);
+        if(idx == -1) {
+            return idx;
+        }
+
+        return idx+1;
+    }
+
+    public int recursiveSearch(int key) {
+        return helper(head,key);
+    }
     public static void main(String[] args) {
         linkedList ll = new linkedList();
         ll.print();
