7c2bc633bd87c0089dd85e872b189a82d73e8ae7
Shubhi89 <shubhidwivedi1415@gmail.com>
2024-12-19 11:26:52 +0530

    using JCF linked list

---
+++ linkedlist/linkedlist_jcf.java
@@ -0,0 +1,21 @@
+package linkedlist;
+
+import java.util.*;
+
+public class linkedlist_jcf {
+    public static void main(String[] args) {
+
+        // create
+        LinkedList<Integer> ll = new LinkedList<>();
+        // add
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addFirst(0);
+
+        System.out.println(ll);
+        // remove
+        ll.removeLast();
+        ll.removeFirst();
+        System.out.println(ll);
+    }
+}
