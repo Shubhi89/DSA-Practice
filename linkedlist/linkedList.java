5fe69f536790da6d98a40ad5f3ee544fbb158932
Shubhi89 <shubhidwivedi1415@gmail.com>
2024-12-19 19:33:04 +0530

    added method of merge sort

--- linkedlist/linkedList.java
+++ linkedlist/linkedList.java
@@ -270,15 +270,65 @@ public class linkedList {
         // remove cycle
         prev.next = null;
     }
+
+    private Node getMid(Node head) {
+        Node slow = head;
+        Node fast = head.next;
+        
+        while( fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow; // mid 
+    }
+
+    private Node merge(Node head1 , Node head2) {
+        Node mergedLL = new Node(-1);
+        Node temp = mergedLL;
+
+        while(head1 != null && head2 != null) {
+            if(head1.data <= head2.data) {
+                temp.next = head1;
+                head1 = head1.next;
+                temp = temp.next;
+            } else {
+                temp.next = head2;
+                head2 = head2.next;
+                temp = temp.next;
+            }
+        }
+        while(head1 != null) {
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
+        }
+        while(head2 != null) {
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;
+        }
+        return mergedLL.next;
+    }
+
+    public Node mergeSort(Node head) {
+        if(head == null || head.next == null) {
+            return head;
+        }
+        // find mid
+        Node mid = getMid(head);
+        // left and right parts
+        Node rightHead = mid.next;
+        mid.next = null;
+        Node newLeft = mergeSort(head);
+        Node newRight = mergeSort(rightHead);
+        // merge
+        return merge(newLeft , newRight);
+    }
     public static void main(String[] args) {
         linkedList ll = new linkedList();
-        ll.print();
         ll.addFirst(1);
-        ll.print();
         ll.addFirst(2);
-        ll.print();
         ll.addLast(4);
-        ll.print();
         ll.addLast(5);
         ll.add(3, 9);
         ll.print();
