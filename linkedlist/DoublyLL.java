afed6c89d2fd274dd90e29485894cf2f69a117bc
Shubhi89 <shubhidwivedi1415@gmail.com>
2024-12-21 11:47:35 +0530

    added method to remove element from DLL

--- linkedlist/DoublyLL.java
+++ linkedlist/DoublyLL.java
@@ -30,7 +30,25 @@ public class DoublyLL {
         head = newNode;
     }
 
-    
+    // remove element
+    public int removeFirst() {
+        if(head == null) {
+            System.out.println("DLL is empty");
+            return Integer.MIN_VALUE;
+        }
+
+        if(size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size--;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        head.prev = null;
+        size--;
+        return val;
+    }
 
     // print 
     public void print() {
