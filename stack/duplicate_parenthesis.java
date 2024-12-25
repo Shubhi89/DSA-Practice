cdf9ae0d25474ab28f7f6ff547cffa0105ff6b30
Shubhi89 <shubhidwivedi1415@gmail.com>
2024-12-25 19:07:03 +0530

    duplicate parenthesis

---
+++ stack/duplicate_parenthesis.java
@@ -0,0 +1,33 @@
+package stack;
+import java.util.*;
+public class duplicate_parenthesis {
+    public static boolean isDuplicate(String str) {
+        Stack<Character> s = new Stack<>();
+
+        for(int i=0;i<str.length();i++) {
+            char ch = str.charAt(i);
+            // closing 
+            if(ch == ')') {
+                int count = 0;
+                while(!s.isEmpty() && s.peek() != '(') {
+                    s.pop();
+                    count++;
+                }
+                if(count < 1) {
+                    return true;
+                } else {
+                    s.pop(); // opening pair
+                }
+            } else {
+                // opening
+                s.push(ch);
+            }
+
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        String str = "((a+b))"; // true;
+        System.out.println(isDuplicate(str));
+    }
+}
