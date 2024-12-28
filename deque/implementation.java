670dfa1c8616f66dab843800cfb87ae243ce5712
Shubhi89 <shubhidwivedi1415@gmail.com>
2024-12-28 10:35:45 +0530

    implementation of deque

---
+++ deque/implementation.java
@@ -0,0 +1,12 @@
+package deque;
+import java.util.*;
+public class implementation {
+    public static void main(String[] args) {
+        Deque<Integer> dq = new LinkedList<>();
+        dq.addFirst(1);
+        dq.addFirst(2);
+        dq.addLast(3);
+        dq.addLast(4);
+        System.out.println(dq);
+    }
+}
