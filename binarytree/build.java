5c45894ed2e2f3375b2376efb711db10e2db0663
Shubhi89 <shubhidwivedi1415@gmail.com>
2025-01-01 12:14:52 +0530

    added method for postorder traversal

--- binarytree/build.java
+++ binarytree/build.java
@@ -46,6 +46,15 @@ public class build {
             System.out.print(root.data+" ");
             inOrder(root.right);
         }
+
+        public static void postOrder(Node root)  {
+            if(root == null) {
+                return;
+            }
+            postOrder(root.left);
+            postOrder(root.right);
+            System.out.print(root.data + " ");
+        }
     }
     public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
@@ -58,5 +67,7 @@ public class build {
         BinaryTree.preOrder(root);
         System.out.println();
         BinaryTree.inOrder(root);
+        System.out.println();
+        BinaryTree.postOrder(root);
     }
 }
