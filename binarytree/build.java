ea09b6ffe97d85dac62c41664a18862bb99046b7
Shubhi89 <shubhidwivedi1415@gmail.com>
2025-01-01 11:59:17 +0530

    added method to preorder traversal

--- binarytree/build.java
+++ binarytree/build.java
@@ -27,6 +27,16 @@ public class build {
 
             return newNode;
         }
+
+        public static void preOrder(Node root) {
+            if(root == null) {
+                System.out.print(-1+ " ");
+                return;
+            }
+            System.out.print(root.data+ " ");
+            preOrder(root.left);
+            preOrder(root.right);
+        }
     }
     public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
@@ -35,5 +45,7 @@ public class build {
         Node root = BinaryTree.buildTree(nodes);
 
         System.out.println(root.data);
+
+        BinaryTree.preOrder(root);
     }
 }
