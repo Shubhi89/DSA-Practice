baf0f7d0812237af7a7777927276b0ac9a81f553
Shubhi89 <shubhidwivedi1415@gmail.com>
2025-01-05 12:37:53 +0530

    approach 2

--- binarytree/lowestCommonAncestor.java
+++ binarytree/lowestCommonAncestor.java
@@ -33,7 +33,7 @@ public class lowestCommonAncestor {
         return false;
     }
 
-    public static Node lca(Node root , int n1 , int n2) { // O(n)
+    public static Node lca(Node root , int n1 , int n2) { // O(n) , O(n)
         ArrayList<Node> path1 = new ArrayList<>();
         ArrayList<Node> path2 = new ArrayList<>();
 
@@ -52,6 +52,28 @@ public class lowestCommonAncestor {
         Node lca = path1.get(i-1);
         return lca;
     }
+
+    // approach 2
+    public static Node lca2(Node root , int n1 , int n2) { //O(n) , O(1)
+        if(root == null) {
+            return null;
+        }
+        if(root.data == n1 || root.data == n2) {
+            return root;
+        }
+
+        Node leftLca = lca2(root.left, n1, n2);
+        Node rightLca = lca2(root.right , n1 , n2);
+
+        if(rightLca == null) {
+            return leftLca;
+        }
+        if(leftLca == null) {
+            return rightLca;
+        }
+
+        return root;
+    }
     public static void main(String[] args) {
         /*
                        1
@@ -69,7 +91,8 @@ public class lowestCommonAncestor {
         root.right.left = new Node(6);
         root.right.right = new Node(7);
 
-        int n1 = 4 , n2 = 7;
+        int n1 = 4 , n2 = 5;
         System.out.println(lca(root, n1, n2).data);
+        System.out.println(lca2(root, n1, n2).data);
     }
 }
