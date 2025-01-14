554fd61eebaff96e1aed30848109b2faecbf6a02
Shubhi89 <shubhidwivedi1415@gmail.com>
2025-01-14 12:01:51 +0530

    added method to delete nodes in BST

--- binarysearchtree/buildTree.java
+++ binarysearchtree/buildTree.java
@@ -52,8 +52,40 @@ public class buildTree {
         }
     }
 
+    public static Node delete(Node root , int val) {
+        if(root.data < val) {
+            root.right = delete(root.right, val);
+        } else if(root.data > val) {
+            root.left = delete(root.left, val);
+        } else {
+            // case 1 = leaf node
+            if(root.left == null && root.right == null) {
+                return null;
+            }
+
+            // case 2 = single child
+            if(root.left == null) {
+                return root.right;
+            } else if(root.right == null) {
+                return root.left;
+            }
+
+            // case 3 = both children
+            Node IS = findInorderSuccessor(root.right);
+            root.data = IS.data;
+            root.right = delete(root.right, IS.data);
+        }
+        return root;
+    }
+
+    public static Node findInorderSuccessor(Node root) {
+        while(root.left != null) {
+            root = root.left;
+        }
+        return root;
+    }
     public static void main(String[] args) {
-        int values[] = {5,1,3,4,2,7};
+        int values[] = {8,5,3,1,4,6,10,11,14};
         Node root = null;
 
         for(int i=0;i<values.length;i++) {
@@ -67,5 +99,8 @@ public class buildTree {
         } else {
             System.out.println("not found");
         }
+
+        root = delete(root, 5);
+        inorder(root);
     }
 }
