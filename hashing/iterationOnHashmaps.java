b816bf74fe62dc0fb2e64615eac65c61dcc25f39
Shubhi89 <shubhidwivedi1415@gmail.com>
2025-01-18 21:13:42 +0530

    iteration on hashmaps

---
+++ hashing/iterationOnHashmaps.java
@@ -0,0 +1,21 @@
+package hashing;
+import java.util.*;
+public class iterationOnHashmaps {
+    public static void main(String[] args) {
+        HashMap<String , Integer> hm = new HashMap<>();
+
+        hm.put("India" , 100);
+        hm.put("China" , 150);
+        hm.put("US" , 50);
+        hm.put("Indonesia" , 6);
+        hm.put("Nepal" , 5);
+
+        // iterate
+        Set<String> keys = hm.keySet();
+        System.out.println(keys);
+
+        for (String k : keys) {
+            System.out.println("key = "+k+" , value = "+hm.get(k));
+        }
+    }
+}
