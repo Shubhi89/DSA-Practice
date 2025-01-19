5ca4ffe63e2eb2982583bb268728c250982ffb50
Shubhi89 <shubhidwivedi1415@gmail.com>
2025-01-19 12:13:24 +0530

    treemap

---
+++ hashing/treeMap.java
@@ -0,0 +1,13 @@
+package hashing;
+import java.util.*;
+public class treeMap {
+    public static void main(String[] args) {
+        TreeMap<String , Integer> tm = new TreeMap<>();
+
+        tm.put("india" , 100);
+        tm.put("china" , 150);
+        tm.put("nepal" , 50);
+
+        System.out.println(tm);
+    }
+}
