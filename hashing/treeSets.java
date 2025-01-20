ad96ae1a421b0e195ba5b6ab8f2432cf64c8550f
Shubhi89 <shubhidwivedi1415@gmail.com>
2025-01-20 11:00:43 +0530

    tree sets

---
+++ hashing/treeSets.java
@@ -0,0 +1,16 @@
+package hashing;
+import java.util.*;
+public class treeSets {
+    public static void main(String[] args) {
+        TreeSet<String> cities = new TreeSet<>();
+
+        cities.add("delhi");
+        cities.add("mumbai");
+        cities.add("kanpur");
+        cities.add("lucknow");
+        cities.add("noida");
+
+        System.out.println(cities);
+
+    }
+}
