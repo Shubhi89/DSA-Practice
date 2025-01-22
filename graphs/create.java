29a3b9b997b109ad466b0db75b0259619ef2fd31
Shubhi89 <shubhidwivedi1415@gmail.com>
2025-01-22 11:50:15 +0530

    added method for BFS on graphs

--- graphs/create.java
+++ graphs/create.java
@@ -12,27 +12,9 @@ public class create {
             this.weight = w;
         }
     }
-    public static void main(String[] args) {
-        /*
-                      (5)
-                0 ----------- 1
-                             / \
-                        (1) /   \ (3)
-                           /     \
-                          2 ----- 3  
-                          |   (1)
-                          |
-                     (2)  |
-                          |
-                          4
-         */
 
-        int vertex = 5;
-        // int arr[] = new arr[vertex]
-        @SuppressWarnings("unchecked")
-        ArrayList<Edge>[] graph = new ArrayList[vertex];
-
-        for(int i=0;i<vertex;i++) {
+    static void createGraph(ArrayList<Edge> graph[]) {
+        for(int i=0;i<graph.length;i++) {
             graph[i] = new ArrayList<>();
         }
 
@@ -55,11 +37,53 @@ public class create {
 
         // 4 -> vertex
         graph[4].add(new Edge(4, 2, 2));
+    }
 
-        // ex: 2's neighbors
-        for(int i=0;i<graph[2].size();i++) {
-            Edge e = graph[2].get(i);
-            System.out.println(e.dest);
+    public static void BFS(ArrayList<Edge>[] graph) { // O(V+E)
+        Queue<Integer> q = new LinkedList<>();
+        boolean visted[] = new boolean[graph.length];
+
+        q.add(0); // source = 0
+
+        while(!q.isEmpty()) {
+            int curr = q.remove();
+            if(!visted[curr]) {
+                System.out.print(curr+" ");
+                visted[curr] = true;
+
+                for(int i=0;i<graph[curr].size();i++) {
+                    Edge e = graph[curr].get(i);
+                    q.add(e.dest);
+                }
+            }
         }
+    }
+    public static void main(String[] args) {
+        /*
+                      (5)
+                0 ----------- 1
+                             / \
+                        (1) /   \ (3)
+                           /     \
+                          2 ----- 3  
+                          |   (1)
+                          |
+                     (2)  |
+                          |
+                          4
+         */
+
+        
+        // ex: 2's neighbors
+        // for(int i=0;i<graph[2].size();i++) {
+        //     Edge e = graph[2].get(i);
+        //     System.out.println(e.dest);
+        // }
+        int vertex = 5;
+        @SuppressWarnings("unchecked")
+        ArrayList<Edge> graph[] = new ArrayList[vertex];
+        createGraph(graph);
+
+        BFS(graph);
     }
 }
