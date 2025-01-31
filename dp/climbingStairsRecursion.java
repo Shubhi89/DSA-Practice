e3c31e59501ec807069b9cca88e29670ace04e9c
Shubhi89 <shubhidwivedi1415@gmail.com>
2025-01-31 11:15:51 +0530

    climbing stairs ques from  recursion

---
+++ dp/climbingStairsRecursion.java
@@ -0,0 +1,17 @@
+package dp;
+
+public class climbingStairsRecursion {
+    public static int ways(int n) {
+        if(n == 0) {
+            return 1;
+        }
+        if(n < 0) {
+            return 0;
+        }
+        return ways(n-1) + ways(n-2);
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(ways(n));
+    }
+}
