3e066170c5057f5c3b105ef656380901da93cad9
Shubhi89 <shubhidwivedi1415@gmail.com>
2025-02-01 11:56:34 +0530

    climbing stairs ques from tabulation

--- dp/climbingStairs.java
+++ dp/climbingStairs.java
@@ -27,11 +27,22 @@ public class climbingStairs {
 
         return dp[n] = waysMemoization(n-1, dp) + waysMemoization(n-2, dp);
     }
+
+    // tabulation = O(n)
+    public static int waysTabulation(int n) {
+        int dp[] = new int[n+1];
+        dp[0] = 1 ; dp[1] = 1;
+        for(int i=2;i<=n;i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
     public static void main(String[] args) {
         int n = 5;
         int dp[] = new int[n+1];
         Arrays.fill(dp , -1);
         System.out.println(waysRecursion(n));
         System.out.println(waysMemoization(n, dp));
+        System.out.println(waysTabulation(n));
     }
 }
