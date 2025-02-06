8b9eb310609a415b138fefb0c0074f9cdef3f18b
Shubhi89 <shubhidwivedi1415@gmail.com>
2025-02-06 17:50:53 +0530

    longest common subsequence ques using tabulation

--- dp/longestCommonSubsequence.java
+++ dp/longestCommonSubsequence.java
@@ -32,6 +32,30 @@ public class longestCommonSubsequence {
             return dp[n][m] =  Math.max(ans1 , ans2);
         }
     }
+
+    public static int lcsTabulation(String str1 , String str2 , int n , int m ) {
+        int dp[][] = new int[n+1][m+1];
+
+        for(int i=0;i<n+1;i++) {
+            dp[i][0] = 0;
+        }
+        for(int j=0;j<m+1;j++) {
+            dp[0][j] = 0;
+        }
+
+        for(int i=1;i<n+1;i++) {
+            for(int j=1;j<m+1;j++) {
+                if(str1.charAt(i-1) == str2.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                } else {
+                    int ans1 = dp[i-1][j];
+                    int ans2 = dp[i][j-1];
+                    dp[i][j] = Math.max(ans1 , ans2);
+                }
+            }
+        }
+        return dp[n][m];
+    }
     public static void main(String[] args) {
         String str1 = "abcdge";
         String str2 = "abedg";
@@ -47,5 +71,6 @@ public class longestCommonSubsequence {
 
         System.out.println(lcsRecursion(str1, str2, str1.length(), str2.length()));
         System.out.println(lcsMemoization(str1, str2, n, m, dp));
+        System.out.println(lcsTabulation(str1, str2, n, m));
     }
 }
