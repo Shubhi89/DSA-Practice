28afe7c1d0211de1cecbbfb766aec5875135cfcc
Shubhi89 <shubhidwivedi1415@gmail.com>
2025-02-13 10:50:19 +0530

    matrix chain multiplication ques using memoization

--- dp/matrixChainMultiplication.java
+++ dp/matrixChainMultiplication.java
@@ -16,10 +16,38 @@ public class matrixChainMultiplication {
         }
         return ans; // mincost
     }
+
+    public static int mcmMemoization(int arr[] , int i , int j , int dp[][]) { // O(n^2)
+        if(i == j) {
+            return 0;
+        }
+
+        if(dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        int ans = Integer.MAX_VALUE;
+        for(int k=i;k<=j-1;k++) {
+            int cost1 = mcmMemoization(arr, i, k, dp);
+            int cost2 = mcmMemoization(arr, k+1, j, dp);
+            int cost3 = arr[i-1] * arr[k] * arr[j];
+            int finalCost = cost1+cost2+cost3;
+            ans = Math.min(ans , finalCost);
+        }
+        return dp[i][j] = ans;
+    }
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,3};
         int n = arr.length;
 
+        int dp[][] = new int[n][n];
+        for(int i=0;i<dp.length;i++) {
+            for(int j=0;j<dp[0].length;j++) {
+                dp[i][j] = -1;
+            }
+        }
+
         System.out.println(mcmRecursion(arr, 1, n-1));
+        System.out.println(mcmMemoization(arr, 1, n-1, dp));
     }
 }
