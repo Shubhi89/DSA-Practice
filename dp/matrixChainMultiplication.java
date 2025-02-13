7c174ba6a16a81e7ab00ba654813fefcbeb3a43b
Shubhi89 <shubhidwivedi1415@gmail.com>
2025-02-13 11:26:46 +0530

    matrix chain multiplication ques using tabulation

--- dp/matrixChainMultiplication.java
+++ dp/matrixChainMultiplication.java
@@ -36,6 +36,29 @@ public class matrixChainMultiplication {
         }
         return dp[i][j] = ans;
     }
+
+    public static int mcmTabulation(int arr[]) {
+        int n = arr.length;
+        int dp[][] = new int[n][n];
+
+        for(int i=0;i<n;i++) {
+            dp[i][i] = 0;
+        }
+
+        for(int len = 2;len <= n-1 ;len++) {
+            for(int i=1;i<=n-len;i++) {
+                int j = i+len-1; // column
+                dp[i][j] = Integer.MAX_VALUE;
+                for(int k=i;k<=j-1;k++) {
+                    int cost1 = dp[i][k];
+                    int cost2 = dp[k+1][j];
+                    int cost3 = arr[i-1] * arr[k] * arr[j];
+                    dp[i][j] = Math.min(dp[i][j] , cost1+cost2+cost3);
+                }
+            }
+        }
+        return dp[1][n-1];
+    }
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,3};
         int n = arr.length;
@@ -49,5 +72,6 @@ public class matrixChainMultiplication {
 
         System.out.println(mcmRecursion(arr, 1, n-1));
         System.out.println(mcmMemoization(arr, 1, n-1, dp));
+        System.out.println(mcmTabulation(arr));
     }
 }
