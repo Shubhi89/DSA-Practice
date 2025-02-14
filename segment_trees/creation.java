19f2121d18829ea4008424d3986b3028ac996838
Shubhi89 <shubhidwivedi1415@gmail.com>
2025-02-14 18:01:12 +0530

    updation on segment trees

--- segment_trees/creation.java
+++ segment_trees/creation.java
@@ -39,6 +39,27 @@ public class creation {
         int n = arr.length;
         return getSumUtil(0, 0, n-1, qi, qj);
     }
+
+    public static void updateUtil(int i , int si , int sj , int idx , int diff) {
+        if(idx > sj || idx <si) { // non-overlapping
+            return;
+        }
+
+        tree[i] += diff;
+        if(si != sj) {
+            int mid = (si+sj)/2;
+            updateUtil(2*i+1, si, mid, idx, diff);// left
+            updateUtil(2*i+2, mid+1, sj, idx, diff); // right
+        }
+    }
+
+    public static void update(int arr[] , int idx , int newVal) { // O(log n)
+        int n = arr.length;
+        int diff = newVal - arr[idx];
+        arr[idx] = newVal;
+
+        updateUtil(0, 0, n-1 , idx , diff);
+    }
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5,6,7,8};
         int n = arr.length;
@@ -52,5 +73,7 @@ public class creation {
         System.out.println();
 
         System.out.println(getSum(arr , 2,5));
+        update(arr, 2, 2);
+        System.out.println(getSum(arr, 2, 5));
     }
 }
