97315bebc37d6f697e33458a516a93cc6c8f5557
Shubhi89 <shubhidwivedi1415@gmail.com>
2025-02-14 17:32:36 +0530

    queries on segment trees

--- segment_trees/creation.java
+++ segment_trees/creation.java
@@ -20,6 +20,25 @@ public class creation {
         tree[sti] = left + right;
         return tree[sti];
     }
+
+    public static int getSumUtil(int i ,int si , int sj ,int qi , int qj) {
+        // case 1
+        if(qj <= si || qi >= sj) { // non-overlapping
+            return 0;
+        } else if(si >= qi && sj <= qj) { // complete overlapping
+            return tree[i];
+        } else { // partial overlap
+            int mid = (si + sj)/2;
+            int left = getSumUtil(2*i+1, si, mid, qi, qj);
+            int right = getSumUtil(2*i+2, mid+1, sj, qi, qj);
+            return left+right;
+        }
+    }
+
+    public static int getSum(int arr[] , int qi , int qj) { // O(log n)
+        int n = arr.length;
+        return getSumUtil(0, 0, n-1, qi, qj);
+    }
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5,6,7,8};
         int n = arr.length;
@@ -31,5 +50,7 @@ public class creation {
             System.out.print(tree[i] +" ");
         }
         System.out.println();
+
+        System.out.println(getSum(arr , 2,5));
     }
 }
