f6b4643c6115980f47722ee261ad07eb33c79d59
Shubhi89 <shubhidwivedi1415@gmail.com>
2025-02-15 12:49:27 +0530

    updation on max Segment trees

--- segment_trees/maxSegmentTree.java
+++ segment_trees/maxSegmentTree.java
@@ -7,7 +7,7 @@ public class maxSegmentTree {
         tree = new int[4*n];
     } 
 
-    public static void buildTree(int i , int si , int sj,int arr[]) {
+    public static void buildTree(int i , int si , int sj,int arr[]) { // O(n)
         if(si == sj) {
             tree[i] = arr[si];
             return;
@@ -32,9 +32,32 @@ public class maxSegmentTree {
         }
     }
 
-    public static int getMax(int arr[] , int qi , int qj) {
+    public static int getMax(int arr[] , int qi , int qj) { // O(log n)
         int n = arr.length;
         return getMaxUtil(0,0,n-1,qi,qj);
+    }
+
+    public static void updateUtil(int i , int si , int sj , int idx , int newVal) {
+        if(idx < si || idx > sj) {
+            return;
+        }
+
+        if(si == sj) {
+            tree[i] = newVal;
+        }
+        
+        if(si != sj) {
+            tree[i] = Math.max(tree[i] , newVal);
+            int mid = (si+sj)/2;
+            updateUtil(2*i+1, si, mid, idx, newVal);
+            updateUtil(2*i+2, mid+1, sj, idx, newVal);
+        } 
+    }
+
+    public static void update(int arr[] , int idx , int newVal) { // O(log n)
+        arr[idx] = newVal;
+        int n = arr.length;
+        updateUtil(0, 0, n-1, idx, newVal);
     }
     public static void main(String[] args) {
         int arr[] = {6,8,-1,2,17,1,3,2,4};
@@ -46,6 +69,10 @@ public class maxSegmentTree {
             System.out.print(tree[i]+" ");
         }
         System.out.println();
+
+        System.out.println(getMax(arr, 2, 5));
+
+        update(arr, 2, 20);
 
         System.out.println(getMax(arr, 2, 5));
     }
