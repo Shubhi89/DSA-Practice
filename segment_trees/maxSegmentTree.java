9880a2c0f88d1e432652baf88a0b90ff8cce1fa9
Shubhi89 <shubhidwivedi1415@gmail.com>
2025-02-15 12:30:09 +0530

    query on max segment tree

--- segment_trees/maxSegmentTree.java
+++ segment_trees/maxSegmentTree.java
@@ -18,6 +18,24 @@ public class maxSegmentTree {
 
         tree[i] = Math.max(tree[2*i+1] , tree[2*i+2]);
     }
+
+    public static int getMaxUtil(int i , int si , int sj , int qi , int qj) {
+        if(si > qj || sj < qi) { // no overlap
+            return Integer.MIN_VALUE;
+        } else if(si >= qi && sj <= qj) { // full overlap
+            return tree[i];
+        } else { // partial overlap
+            int mid = (si + sj)/2;
+            int left = getMaxUtil(2*i+1, si, mid, qi, qj);
+            int right = getMaxUtil(2*i+2, mid+1, sj, qi, qj);
+            return Math.max(left , right);
+        }
+    }
+
+    public static int getMax(int arr[] , int qi , int qj) {
+        int n = arr.length;
+        return getMaxUtil(0,0,n-1,qi,qj);
+    }
     public static void main(String[] args) {
         int arr[] = {6,8,-1,2,17,1,3,2,4};
         int n = arr.length;
@@ -28,5 +46,7 @@ public class maxSegmentTree {
             System.out.print(tree[i]+" ");
         }
         System.out.println();
+
+        System.out.println(getMax(arr, 2, 5));
     }
 }
